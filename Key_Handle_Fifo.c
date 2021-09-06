f9dc6dc1a05ddde8b6db563fb0860aa621332cac
super1044929595 <1044929595@qq.com>
2021-09-07 00:49:43 +0800

    Update Key_Handle_Fifo.c

--- Key_Handle_Fifo.c
+++ Key_Handle_Fifo.c
@@ -1,80 +1,111 @@
-#include "Key_Handle_Fifo.h"
+#include "Key_Handle_Middle.h"
 
+
+//public
+//---------------------------------------------------------------------
+#define TOUCH_FILETR_USE_LENGTH           5U
+
+#define TOUCH_FILTER_PUSHNPSAME           1U
 //private 
 //---------------------------------------------------------------------
-#define TOUCH_FILETR_LENGTH              (64U)  // the used is only 20 
-static  uint8_t   xTouch_Filter_Value[TOUCH_FILETR_LENGTH];
-static  uint8_t   xTouch_Filter_Status[TOUCH_FILETR_LENGTH];
-static  uint8_t   xTouch_WCnt=0;
-static  uint8_t   xTouch_RCnt=0;
-static  uint8_t   xTouch_cnt=0;
-static  uint16_t  xTouch_Pre;
-
-static bool	Press_StoreFull(void);
-static bool Press_StoreZero(void);
-
+#define TOUCH_FILETR_LENGTH              (TOUCH_FILETR_USE_LENGTH+1)  // the used is only 20 
+static  unsigned char   xTouch_Filter_Value[TOUCH_FILETR_USE_LENGTH];
+static  unsigned char   xTouch_Filter_Status[TOUCH_FILETR_USE_LENGTH];
+static  unsigned char   xTouch_WCnt = 0;
+static  unsigned char   xTouch_RCnt = 0;
+static  unsigned char   xTouch_cnt = 0;
+static  unsigned short  xTouch_Pre = 0;
+static unsigned char	Press_StoreFull(void);
+static unsigned char    Press_StoreZero(void);
 //---------------------------------------------------------------------
 
 
-bool	Press_StoreFull(void)
-{	
-    if((xTouch_WCnt==xTouch_RCnt)&&(xTouch_cnt == TOUCH_FILETR_LENGTH))
-    {	
-        //xTouch_cnt = 0;
-        return true;
+unsigned char	Press_StoreFull(void)
+{ 
+    if ((( xTouch_WCnt+1) % TOUCH_FILETR_LENGTH) == xTouch_RCnt){
+         return 1;
+    }
+    else if (xTouch_cnt == TOUCH_FILETR_LENGTH) {
+         return 1;
     }
     else {
-        return false;
+        return 0;
     }
 }
 
-bool    Press_StoreZero(void)
-{	
-    if((0== xTouch_cnt)&&(xTouch_WCnt== xTouch_RCnt))
-    {	
-        return true;	
+unsigned char    Press_StoreZero(void)
+{	  
+    if (xTouch_RCnt% TOUCH_FILETR_LENGTH == xTouch_WCnt) {
+        return 1;
     }
-    else
-        return false;
+    else {
+        return 0;
+    }
 }
 //public method
 void   Press_KeyValueInit(void)
 {	
-    xTouch_WCnt=(int8_t)0;
-    xTouch_RCnt=(int8_t)0;
-    memset((char*)xTouch_Filter_Value,0,sizeof(xTouch_Filter_Value));		
-    memset((char*)xTouch_Filter_Status,0,sizeof(xTouch_Filter_Status));
+    xTouch_WCnt=(unsigned char)0;
+    xTouch_RCnt=(unsigned char)0;
+    xTouch_cnt =(unsigned char)0;
+    memset((unsigned char*)xTouch_Filter_Value,0,sizeof(xTouch_Filter_Value));		
+    memset((unsigned char*)xTouch_Filter_Status,0,sizeof(xTouch_Filter_Status));
 }
 
-bool 	Press_KeyValueWrite(uint8_t prevalue,uint8_t prestatus)
+
+unsigned short last_KeyValue[2] = { 0,0 };
+unsigned char 	Press_KeyValueWrite(unsigned char prevalue,unsigned char prestatus)
 {	
-    uint16_t _key_pre;	
     if(Press_StoreFull())
     {	
-        return true;
-    }	
-    XTOUCH_KEYVALUE_SENDMESSAGE(_key_pre,prevalue,prestatus);	
-    if(_key_pre==xTouch_Pre)	
-        return true;	
-    xTouch_Filter_Value[xTouch_WCnt]=prevalue;		
-    xTouch_Filter_Status[xTouch_WCnt]= prestatus;
+        return 1;
+    }   
+    #ifdef TOUCH_FILTER_PUSHNPSAME //cancel by user when not need the difference of the same button value 	
+    XTOUCH_KEYVALUE_SENDMESSAGE(last_KeyValue[1],prevalue,prestatus);
+   
+    if(last_KeyValue[1]== last_KeyValue[0])
+        return 1;
+    #endif	
+    
+    xTouch_Filter_Value[xTouch_WCnt]=(unsigned char)prevalue;		
+    xTouch_Filter_Status[xTouch_WCnt]= (unsigned char)prestatus;
     XTOUCH_KEYVALUE_SENDMESSAGE(xTouch_Pre,prevalue,prestatus);
-    xTouch_WCnt=(++xTouch_WCnt)%TOUCH_FILETR_LENGTH;	
-    xTouch_cnt=++xTouch_cnt;
+    xTouch_WCnt = xTouch_WCnt++;
+    xTouch_WCnt=(unsigned char)(xTouch_WCnt%TOUCH_FILETR_LENGTH);	
+    ++xTouch_cnt ;
+    last_KeyValue[0] = last_KeyValue[1];
+    return 0;
 }
 
-uint16_t  Press_KeyValueRead(void)
+unsigned short  Press_KeyValueRead(void)
 {	
-    uint16_t _key_pre;  
+    unsigned short _key_pre;  
     if(Press_StoreZero())
     {
-        return true;
-    }		
-
+        return 1;
+    }		   
     XTOUCH_KEYVALUE_SENDMESSAGE(_key_pre,xTouch_Filter_Value[xTouch_RCnt],xTouch_Filter_Status[xTouch_RCnt]);
-    xTouch_Filter_Value[xTouch_RCnt] = (uint8_t)0;
-    xTouch_Filter_Status[xTouch_RCnt]= (Touch_Press_Enum)TOUCH_PRESS_UNKNOW;
-    xTouch_RCnt =(++xTouch_RCnt)%TOUCH_FILETR_LENGTH;
-    xTouch_cnt=--xTouch_cnt;	
+    xTouch_Filter_Value[xTouch_RCnt] = (unsigned char)0;
+    xTouch_Filter_Status[xTouch_RCnt]= (unsigned char)TOUCH_PRESS_UNKNOW;
+    ++xTouch_RCnt;
+    xTouch_RCnt =(unsigned char)(xTouch_RCnt%TOUCH_FILETR_LENGTH);
+    --xTouch_cnt;	
     return _key_pre;
+}
+
+
+unsigned short Press_KeyLoop(void)
+{
+    unsigned short key = 0;
+    unsigned short keyfact = 0;
+
+   
+    for (int i = 0; i <6; i++) {
+        key=Press_KeyValueRead();
+        if ((key&0xff) == (unsigned short)TOUCH_PRESS_DOWN) {
+            ++keyfact;
+        }
+    }
+   
+    return keyfact;
 }
