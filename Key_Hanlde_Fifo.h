c1f725b6bf14916a4ec56865b4072121e3ad55ef
super1044929595 <1044929595@qq.com>
2021-09-07 00:50:29 +0800

    Update Key_Hanlde_Fifo.h

--- Key_Hanlde_Fifo.h
+++ Key_Hanlde_Fifo.h
@@ -1,30 +1,40 @@
-#ifndef __KEY_HANDLE_FIFO_H
-#define __KEY_HANDLE_FIFO_H
+#ifndef __KEY_HANDLE_MIDDLE_H
+#define __KEY_HANDLE_MIDDLE_H
 
 #ifdef __cplusplus
 	extern "C"{
 #endif
 
-#define XTOUCH_KEYVALUE_SENDMESSAGE(xvalue,value,status)    ((uint16_t)xvalue=(uint16_t)(((value&0xff)<<8)|(status&0xff)))
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+//#include "FreeRTOS.h"
+//#include "task.h"
+
+//typedef unsigned char  uint8_t;
+//typedef unsigned short uint16_t;
+
+#define XTOUCH_KEYVALUE_SENDMESSAGE(xvalue,value,status)    (xvalue=((value&0xff)<<8)|(status&0xff))
 
 typedef enum{
 
 	TOUCH_PRESS_UNKNOW=0x00U,
 	TOUCH_PRESS_UP,
 	TOUCH_PRESS_DOWN,
-	TOUCH_PRESS_LONGTIME,
-	
+	TOUCH_PRESS_DOUBLE,
+	TOUCH_PRESS_THREE,
+	TOUCH_PRESS_LONGTIEM,
+
 }Touch_Press_Enum;
 
 
 //public method
 void      Press_KeyValueInit(void);
-bool 	  Press_KeyValueWrite(uint8_t prevalue,uint8_t prestatus);
-uint16_t  Press_KeyValueRead(void);
-
-
-
-
+unsigned char 	  Press_KeyValueWrite(unsigned char prevalue,unsigned char  prestatus);
+unsigned short  Press_KeyValueRead(void);
+unsigned short  Press_KeyLoop(void);
 
 
 
